9b2b8c1d6a58b89d5a74820b1908e1daa5d16d21
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-10-16 19:36:34 +0530

    My First Commit

---
+++ Namaskar.java
@@ -0,0 +1,24 @@
+class Wish
+{
+//To start a program we need a method(main)
+//main method is an entry point
+public static void main(String[] param)
+{
+System.out.println("Anita");
+}
+}
+
+class Camera
+{
+public static void main(String[] camera){
+System.out.println("Avi");
+System.out.println("nash");
+}
+}
+
+class Weather
+{
+public static void main(String[] weather){
+System.out.println("Gadag");
+}
+}
