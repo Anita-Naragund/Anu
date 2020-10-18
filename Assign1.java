24b4463de36589694397fee6815388d7a88e994b
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-10-18 19:52:35 +0530

    My Second Commit

---
+++ Assign1.java
@@ -0,0 +1,17 @@
+class Assignment
+{
+public static void main(String[] args)
+{
+System.out.println("aaa");
+music();
+drinking();
+}
+public static void music()
+{
+System.out.println("I love music");
+}
+public static void drinking()
+{
+System.out.println("Coffee");
+}
+}
---
+++ Assign2.java
@@ -0,0 +1,32 @@
+class Travel
+{
+public static void main(String params[])
+{
+System.out.println("I love travelling");
+shimla();
+bankok();
+malaysia();
+kashmir();
+kanyakumari();
+}
+public static void shimla()
+{
+System.out.println("Shimla");
+}
+public static void bankok()
+{
+System.out.println("Bankok");
+}
+public static void malaysia()
+{
+System.out.println("Malaysia");
+}
+public static void kashmir()
+{
+System.out.println("Kashmir");
+}
+public static void kanyakumari()
+{
+System.out.println("Kanyakumari");
+}
+}
