c6580ca76d31de287e55228883c6b621d9d93583
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-10-19 23:40:52 +0530

    My Third Commit

---
+++ Fruit.java
@@ -0,0 +1,101 @@
+public class Fruit
+{
+public static void main(String args[])
+{
+short daysInYear;
+byte fingers;
+int numberOfEngineersInIndia;
+long populationOfIndia;
+float percentageOfStudent;
+double chargeOfElectrons;
+boolean earthIsFlat;
+char grade;
+String name;
+daysInYear=365;
+fingers=5;
+numberOfEngineersInIndia=2900000;
+populationOfIndia=1350000000;
+percentageOfStudent=7;
+chargeOfElectrons=1.60217662;
+earthIsFlat=false;
+grade='A';
+name="Anita";
+System.out.println(daysInYear);
+System.out.println(fingers);
+System.out.println(numberOfEngineersInIndia);
+System.out.println(populationOfIndia);
+System.out.println(percentageOfStudent);
+System.out.println(chargeOfElectrons);
+System.out.println(earthIsFlat);
+System.out.println(grade);
+System.out.println(name);
+FirstVariable();
+SecondVariable();
+ThirdVariable();
+}
+
+
+public static void FirstVariable(){
+short minutesInADay=1440; 
+byte membersInAHouse=35;
+int numberOfIndianSoldiers=1237117;
+long oneMillionInRupees=73358800;
+float valueOfPie=3;
+double massOfNeutron=1.67493;
+boolean foodIsTasty=true;
+char attendence='P';
+String address= "Aravind Nagar Hubli";
+System.out.println("...................");
+System.out.println(minutesInADay);
+System.out.println(membersInAHouse);
+System.out.println(numberOfIndianSoldiers);
+System.out.println(oneMillionInRupees);
+System.out.println(valueOfPie);
+System.out.println(massOfNeutron);
+System.out.println(foodIsTasty);
+System.out.println(attendence);
+System.out.println(address);
+}
+public static void SecondVariable(){
+short statesInKarnataka=30;
+byte hoursInADay=24;
+int populationOfKarnataka=70000000;
+long oneGoogolEqualTo=271828723;
+float eulersNumber=2;
+double weightOfEarth=5.97225424;
+boolean subjectIsInteresting=true;
+char result='P';
+String salary="52000";
+System.out.println("...................");
+System.out.println(statesInKarnataka);
+System.out.println(hoursInADay);
+System.out.println(populationOfKarnataka);
+System.out.println(oneGoogolEqualTo);
+System.out.println(eulersNumber);
+System.out.println(weightOfEarth);
+System.out.println(subjectIsInteresting);
+System.out.println(result);
+System.out.println(salary);
+}
+public static void ThirdVariable(){
+short totalDistricsInKarataka=30;
+byte numberOfPlanets=8;
+int totalIndiansInUsa=2843391;
+long distanceOfSunFromEarth=148980000;
+float interestPaidForLoan=3;
+double weightOfElectron=9.1093837015;
+boolean youAreHappy=false;
+char a='9';
+String jobTitle="Software developer";
+System.out.println("...................");
+System.out.println(totalDistricsInKarataka);
+System.out.println(numberOfPlanets);
+System.out.println(totalIndiansInUsa);
+System.out.println(distanceOfSunFromEarth);
+System.out.println(interestPaidForLoan);
+System.out.println(weightOfElectron);
+System.out.println(youAreHappy);
+System.out.println(a);
+System.out.println(jobTitle);
+}
+}
